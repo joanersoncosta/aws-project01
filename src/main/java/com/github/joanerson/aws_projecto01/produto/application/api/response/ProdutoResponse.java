636cadaa76fdae26d1f9254fa478a8c2efdbc2fe
Joanerson Costa <joanersonc31@gmail.com>
2024-10-15 19:04:30 -0300

    feat: valida produto ja criado na camada infra

---
+++ src/main/java/com/github/joanerson/aws_projecto01/produto/application/api/response/ProdutoResponse.java
@@ -0,0 +1,4 @@
+package com.github.joanerson.aws_projecto01.produto.application.api.response;
+
+public class ProdutoResponse {
+}
--- src/main/java/com/github/joanerson/aws_projecto01/produto/infra/ProdutoInfraRepository.java
+++ src/main/java/com/github/joanerson/aws_projecto01/produto/infra/ProdutoInfraRepository.java
@@ -1,9 +1,11 @@
 package com.github.joanerson.aws_projecto01.produto.infra;
 
+import com.github.joanerson.aws_projecto01.handler.APIException;
 import com.github.joanerson.aws_projecto01.produto.application.repository.ProdutoRepository;
 import com.github.joanerson.aws_projecto01.produto.domain.Produto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class ProdutoInfraRepository implements ProdutoRepository {
     private final ProdutoSpringDataJpaRepository produtoSpringDataJpaRepository;
+
     @Override
     public Produto salva(Produto produto) {
         log.info("[start] ProdutoInfraRepository - salva");
@@ -18,9 +21,9 @@ public class ProdutoInfraRepository implements ProdutoRepository {
             produtoSpringDataJpaRepository.save(produto);
         } catch (Exception e) {
             log.info("Erro ao salvar produto: {}", e.getMessage());
-            throw new RuntimeException(e);
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Este produto já existe", e);
         }
-            log.info("[finish] ProdutoInfraRepository - salva");
+        log.info("[finish] ProdutoInfraRepository - salva");
         return produto;
     }
 }
